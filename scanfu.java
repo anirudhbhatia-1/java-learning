8d3dce5bcaf1b43757841a759bdc7feb19e29eb7
Anirudh Bhatia <aanirudhbhatia@gmail.com>
2026-08-20 11:36:02 +0530

    learning scanner function

--- scanfu.java
+++ scanfu.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 public class scanfu{
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
-        System.out.println("Enter your name :");
+        //System.out.print used when we use the same line
+        System.out.print("Enter your name :");
         String name= sc.nextLine();
-        System.out.println("Enter your age:");
+        System.out.print("Enter your age:");
         int age=sc.nextInt();
-        System.out.println("enter your targate weight");
+        System.out.print("enter your targate weight");
         double wei=sc.nextDouble();
+        //System.out.println is used when we need to change the line
         System.out.println("your nbame is:"+name);
         System.out.println("your age after 5 years:"+ (age+5) );
         System.out.println("your targate weightis :"+wei);
